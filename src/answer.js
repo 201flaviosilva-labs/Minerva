38db3ec2d82d8e1980288bd3adf2f2eac50fcfef
Flavio Silva <flavioandre.2001.9.7@gmail.com>
2020-09-18 21:00:53 +0100

    Update answer.js

--- src/answer.js
+++ src/answer.js
@@ -1,4 +1,5 @@
 export function answer(resp) {
 	if (resp === "olá" || resp === "ola" || resp === "hey" || resp === "hey there") console.log("Hey there");
 	if (resp === "atão" || resp === "atao" || resp === "atao?" || resp === "atão?") console.log("Atão era pastor e comia batatas á bugidor");
+
 }
